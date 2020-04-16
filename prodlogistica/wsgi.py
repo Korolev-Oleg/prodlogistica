1fac8c5caf8157daf16e79889b3cb04a870a5bdb
uston <1kor.oleg@gmail.com>
2020-04-16 21:54:56 +0300

    update wsgi

--- prodlogistica/wsgi.py
+++ prodlogistica/wsgi.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 WSGI config for django_cms project.
 
@@ -8,11 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os, sys
+import os
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.insert(0, '/var/www/u0721006/data/www/prodlogistica.ru/prodlogistica')
-sys.path.insert(1, '/var/www/u0721006/data/www/venv/lib/python3.7/site-packages')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prodlogistica.settings')
+
 application = get_wsgi_application()
