3d0baddbbeac498da7d146591cdd91c819882eab
uston <1kor.oleg@gmail.com>
2020-04-26 20:44:37 +0300

    config gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,10 +1,8 @@
-command = '/home/www/projects/env/bin/gunicorn'
-
-command = '/home/www/code/project/env/bin/gunicorn'
-pythonpath = '/home/www/code/project/project'
+command = '/home/www/prodlogistica.ru/env/bin/gunicorn'
+pythonpath = '/home/www/prodlogistica.ru/project'
 bind = '127.0.0.1:8001'
 workers = 3
 user = 'www'
 limit_request_fields = 32000
 limit_request_field_size = 0
-raw_env = 'DJANGO_SETTINGS_MODULE=project.settings'
+raw_env = 'DJANGO_SETTINGS_MODULE=project.config'
