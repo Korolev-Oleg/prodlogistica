659c4d275db2649b9a01d9a26331717a27b7f3c5
uston <1kor.oleg@gmail.com>
2020-04-26 20:26:13 +0300

    update structure

---
+++ gunicorn_config.py
@@ -0,0 +1,10 @@
+command = '/home/www/projects/env/bin/gunicorn'
+
+command = '/home/www/code/project/env/bin/gunicorn'
+pythonpath = '/home/www/code/project/project'
+bind = '127.0.0.1:8001'
+workers = 3
+user = 'www'
+limit_request_fields = 32000
+limit_request_field_size = 0
+raw_env = 'DJANGO_SETTINGS_MODULE=project.settings'
