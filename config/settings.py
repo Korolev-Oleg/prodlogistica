b922fabe24ab0964dc7f8b1703a57bcd132599c6
uston <1kor.oleg@gmail.com>
2020-04-27 19:47:22 +0300

    update database config

--- config/settings.py
+++ config/settings.py
@@ -226,9 +226,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'prodlogistica',
-        'USER': 'postgres',
-        'PASSWORD': 't!()N20ms',
-        # 'PASSWORD': 'prodlog_t!()N20ms',
+        'USER': 'dbms',
+        'PASSWORD': 'prodlog_t!()N20ms',
         'HOST': 'localhost',
         'PORT': 5432
     }
