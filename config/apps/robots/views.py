ad14eaa0005cebfe1170aa731b155d9afb3ceadc
uston <1kor.oleg@gmail.com>
2020-04-30 14:29:02 +0300

    fix robot.txt

--- config/apps/robots/views.py
+++ config/apps/robots/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from django.http import HttpResponse
 
 HttpResponse('', content_type='html')
 
 
-# Create your views here.
 def robots(request):
-    request.get_full_path_info()
-    return render_to_response('robots.txt {{ STATIC_URL }}',
-                              content_type="text/plain")
+    return render(request, 'robots.txt', content_type="text/plain")
