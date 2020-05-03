aad3da607010c61846b23cff5b71b9a96adbbc83
uston <1kor.oleg@gmail.com>
2020-05-03 19:46:33 +0300

    set production

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
