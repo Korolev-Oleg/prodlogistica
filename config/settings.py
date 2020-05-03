28f774a677e3be36f04eca327bb5a1c3d3026f73
uston <1kor.oleg@gmail.com>
2020-05-03 22:31:05 +0300

    set degug

--- config/settings.py
+++ config/settings.py
@@ -222,15 +222,25 @@ CMS_PLACEHOLDER_CONF = {}
 if DEBUG:
     DATABASES = {
         'default': {
-            'CONN_MAX_AGE': 0,
-            'ENGINE': 'django.db.backends.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'prodlogistica',
+            'USER': 'dbms',
+            'PASSWORD': 'prodlog_t!()N20ms',
             'HOST': 'localhost',
-            'NAME': 'project.db',
-            'PASSWORD': '',
-            'PORT': '',
-            'USER': ''
+            'PORT': 5432
         },
     }
+    # DATABASES = {
+    #     'default': {
+    #         'CONN_MAX_AGE': 0,
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'HOST': 'localhost',
+    #         'NAME': 'project.db',
+    #         'PASSWORD': '',
+    #         'PORT': '',
+    #         'USER': ''
+    #     },
+    # }
 else:
     DATABASES = {
         'default': {
