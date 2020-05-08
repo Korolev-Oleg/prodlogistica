3613f84d988449c5bc7addbe1c2869c7f585bdca
uston <1kor.oleg@gmail.com>
2020-05-08 19:19:04 +0300

    add contacts page

--- config/settings.py
+++ config/settings.py
@@ -17,8 +17,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-SERVER_DB = True
+DEV = False
+DEBUG = DEV
+SERVER_DB = not DEV
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -95,7 +96,7 @@ STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'config', 'static'),
-    '/home/www/prodlogistica.ru/project',
+    # '/home/www/prodlogistica.ru/project',
 )
 SITE_ID = 1
 
