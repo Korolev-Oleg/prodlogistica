49fd309e5c5625791200936f43b1d135de8c6a73
hustinCun <1kor.oleg@gmail.com>
2020-08-03 17:05:16 +0300

    add dev server

---
+++ dev_gunicorn_config.py
@@ -0,0 +1,8 @@
+command = '/home/www/prodlogistica.ru/env/bin/gunicorn'
+pythonpath = '/home/www/prodlogistica.ru/project'
+bind = '127.0.0.1:8005'
+workers = 3
+user = 'www'
+limit_request_fields = 32000
+limit_request_field_size = 0
+raw_env = 'DJANGO_SETTINGS_MODULE=config.settings.production'
