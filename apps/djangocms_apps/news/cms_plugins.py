fd5361a1b2f426a4f3f25ce03ffa774aa27c72b5
hustinCun <1kor.oleg@gmail.com>
2020-08-05 19:13:31 +0300

    develop apps catalog, news

---
+++ apps/djangocms_apps/news/cms_plugins.py
@@ -0,0 +1,23 @@
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from cms.models.pluginmodel import CMSPlugin
+from django.utils.translation import ugettext_lazy as _
+
+from .models import Vacancy
+
+
+@plugin_pool.register_plugin
+class NewsPlugin(CMSPluginBase):
+    model = CMSPlugin
+    render_template = "news_plugin.html"
+    name = 'Блок новостей'
+    cache = False
+
+    def render(self, context, instance, placeholder):
+        context.update(
+            {
+                'instance': instance,
+                'vacancy': Vacancy.objects.all()
+            }
+        )
+        return context
