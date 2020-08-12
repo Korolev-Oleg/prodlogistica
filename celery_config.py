db19574388f156faf90eb36a364af47addba09c5
hustinCun <1kor.oleg@gmail.com>
2020-08-12 17:15:15 +0300

    fix typo

--- celery_config.py
+++ celery_config.py
@@ -4,5 +4,5 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.develop')
 
 app = Celery('config')
-app.config_from_object('django.cond:settings')
+app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
