2f081a7b0c476559a00b3d00e6633c316c54a606
hustinCun <1kor.oleg@gmail.com>
2020-08-17 14:05:32 +0300

    remake hello message

--- frontend/js/lib/hello.js
+++ frontend/js/lib/hello.js
@@ -1,1 +1,1 @@
-console.log('%c      WELCOME!\nFull Stack Developer\ngitlab:hustoncun\nemail:hustoncun@ya.ru', 'color: #31Ac53; font-size: x-large; font-weight: bold; margin-left: 43%;');
+console.log('%c      WELCOME!\nFull Stack Developer\ngithub:hustoncun\nemail:ok@prodlg.ru', 'color: #31Ac53; font-size: x-large; font-weight: bold; margin-left: 43%;');
