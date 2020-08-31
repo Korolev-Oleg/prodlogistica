9f2602e0bdd4d2ff59b6f8a86d9b89baf480318c
hustinCun <1kor.oleg@gmail.com>
2020-08-31 13:58:34 +0300

    refactoring and add "function get max page height and set this to #white-cover block"

---
+++ frontend/js/lib/preloader.js
@@ -0,0 +1,23 @@
+/**
+ * Preloader function
+ */
+$(window).on('load', function onload() {
+  const $preloader = $('.preloader');
+  const svgAnm = $preloader.find('.svg_anm');
+  svgAnm.fadeOut();
+  $preloader.delay(1).fadeOut('fast');
+  setWhiteCoverWidth()
+});
+
+/**
+ * function get max page height and set this to #white-cover block
+ */
+function setWhiteCoverWidth(){
+  let body = document.body,
+      html = document.documentElement;
+
+  let pageHeight = Math.max( body.scrollHeight, body.offsetHeight,
+                             html.clientHeight, html.scrollHeight, html.offsetHeight );
+  document.getElementById('white-cover').style.height = String(pageHeight) + 'px'
+  console.log('set white-cover height', pageHeight)
+}
