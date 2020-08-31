8a64e94bacad95238dfed74ae8d1f81f9babb4d5
hustinCun <1kor.oleg@gmail.com>
2020-08-31 13:58:04 +0300

    remove

--- frontend/js/lib/config.js
+++
@@ -1,6 +0,0 @@
-$(window).on('load', function onload() {
-  const $preloader = $('.preloader');
-  const svgAnm = $preloader.find('.svg_anm');
-  svgAnm.fadeOut();
-  $preloader.delay(1).fadeOut('fast');
-});
