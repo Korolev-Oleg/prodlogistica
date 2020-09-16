af6c0a5e95e6b689a2231525e15684f62574cc1a
Hustoncun <1kor.oleg@gmail.com>
2020-09-16 16:51:20 +0300

    remove RSA; add global attributes; change StatusCodes; add hot_start in to IntegrationController; change __preparing__ method; make __upload_token__ method; morph self.status_codes -> StatusCodes; automate __is_status_code_correct__ method; make start method.

--- apps/integration_1S/tasks.py
+++ apps/integration_1S/tasks.py
@@ -1,170 +1,144 @@
-"""
-Integration module with 1C server
-flask on 1s server host:port: 77.37.174.39:33896
-
-the module starts unloading the database from the 1C server
-"""
-
-from celery_config import app
-from requests import post
-
-import rsa
-from redis import Redis
-from dependency_injector import containers
-from dependency_injector import providers
 from dataclasses import dataclass
+from random import choices
+from string import ascii_letters
 
-SERVER_1S_HOST = "77.37.174.39:33896/start_uploading"
+from redis import Redis
+import requests
+
+PROTOCOL = 'https'
+# SERVER_1S = '77.37.174.39:33896'
+SERVER_1S = '127.0.0.1:5001'
+ACTION = 'start_uploading'
+
+HOST_1S = f'{PROTOCOL}://{SERVER_1S}/{ACTION}'
+
+
+def get_from_redis(key_name: str) -> any:
+    return Redis().get(key_name).decode('utf-8')
 
 
 @dataclass
 class StatusCodes:
-    uploading = 'uploading'
-    connection = 'connection'
-    uploaded = 'uploaded'
-    null = 'nil'
+    connection_failed = 'Connection to the 1s server failed.'
+    connection_to_1C_server = 'Start connection to the 1s server.'
+    connected = 'Connected to 1S server success, start integration.'
+    uploading = 'Uploading from 1S database started.'  # when flask in {HOST_1S} send data to the {HOST}/api
+    uploaded = 'Integration complete.'  # when uploading finished
+    null = None  # when redis restarted
     key_name = '1CIntegrationStatusCode'
 
 
 @dataclass
-class RSAKeys:
-    n: int  # pk + prk
-    e: int  # pk + prk
-    d: int  # prk
-    p: int  # prk
-    q: int  # prk
-    public: object
-    private: object
-    key_name_n = '1CPublicRSAn'
-    key_name_e = '1CPublicRSAe'
-    key_name_d = '1CPublicRSAd'
-    key_name_p = '1CPublicRSAp'
-    key_name_q = '1CPublicRSAq'
+class Exceptions:
+    status_code_error = Exception('1S integration status code is incorrect')
+    connection_error = Exception(f'Connection to the {HOST_1S} error')
+    wrong_token = Exception('Wrong token value')
+
+
+@dataclass
+class SuccessToken:
+    key_name = '1CIntegrationSecretKey'
+    json_key = 'token'
+    value: str
 
 
 class IntegrationController:
-    """
-    The module, upon initialization, determines the unloading status and creates
-    a key pair that implements encryption using the RSA method. Further, the
-    status and keys are saved in the redis database. At the end, the module
-    sends a POST request to the unloading module address on the 1C side, which
-    contains information about the public key.
-    The module also provides a method for decrypting data, their verification is
-    carried out by the hash sum
-    """
     status_codes = StatusCodes
-    status_code_error = Exception('StatusCodeError')
-    encrypt_bits: int
+    secret_key = SuccessToken
     uploaded_at: str
     status: str
-    rsa_keys = RSAKeys
 
-    def __init__(self, encrypt_bits=512):
+    def __init__(self, hot_start=False, bits=512):
         self.redis = Redis()
         self.status = self.__get_status__()
-        self.encrypt_bits = encrypt_bits
+        self.secret_key_bits = bits
         self.__preparing__()
 
-    def __preparing__(self):
+        if hot_start:
+            self.start()
+
+    def __preparing__(self) -> None:
         if self.__is_status_code_correct__(self.status):
-            is_status_uploaded = self.status == self.status_codes.uploaded
-            is_status_null = self.status == self.status_codes.null
-            if is_status_uploaded or is_status_null:
-                self.set_status(self.status_codes.connection)
-                self.__render_new_keys__()
+            is_uploaded = self.status == StatusCodes.uploaded
+            is_null = self.status == StatusCodes.null
+            is_failed = self.status == StatusCodes.connection_failed
+            if is_uploaded or is_null or is_failed:
+                self.__save_status_to_redis__(StatusCodes.connection_to_1C_server)
+                self.__new_token__()
             else:
-                self.__get_keys__()
+                self.__upload_token__()
 
-        else:
-            raise IOError(self.status_code_error)
+    def __new_token__(self) -> None:
+        """ Create new token and save it in redis """
+        SuccessToken.value = ''.join(choices(ascii_letters, k=self.secret_key_bits))
+        self.redis.set(name=SuccessToken.key_name, value=SuccessToken.value)
 
-    def start(self) -> None:
-        # TODO дописать запрос на серевер с выгрузкой ключей
-        if self.status == self.status_codes.connection:
-            json = {
-                    'keys': {
-                        'n': self.rsa_keys.n,
-                        'e': self.rsa_keys.e,
-                    },
-                    'action': self.status_codes.uploading
-                }
-            post(url=SERVER_1S_HOST, json=json)
+    def __upload_token__(self) -> None:
+        """ Load SuccessToken.value from redis  """
+        SuccessToken.value = self.redis.get(SuccessToken.key_name).decode('utf-8')
 
     def __get_status__(self) -> str:
         """
         load status from redis db
         @return: str StatusCodes.status_code
         """
-        status_code = self.redis.get(self.status_codes.key_name)
+        status_code = self.redis.get(StatusCodes.key_name)
         if status_code:
             return status_code.decode('utf-8')
         else:
-            return self.status_codes.null
+            return StatusCodes.null
 
-    def __get_keys__(self) -> None:
-        """ Load keys from redis """
-        def get(key):
-            return int(self.redis.get(key).decode('utf-8'))
+    @staticmethod
+    def __is_status_code_correct__(status_code: str) -> bool:
+        """ Checks if status_code exists in StatusCodes.__dict__"""
+        for attribute in StatusCodes.__dict__:
+            if not attribute.startswith('__') and not attribute.endswith('__'):
+                if status_code == StatusCodes().__getattribute__(attribute):
+                    return True
 
-        self.rsa_keys.n = get(self.rsa_keys.key_name_n)
-        self.rsa_keys.e = get(self.rsa_keys.key_name_e)
-        self.rsa_keys.d = get(self.rsa_keys.key_name_d)
-        self.rsa_keys.p = get(self.rsa_keys.key_name_p)
-        self.rsa_keys.q = get(self.rsa_keys.key_name_q)
+        raise IOError(Exceptions.status_code_error)
 
-        self.rsa_keys.public = rsa.PublicKey(
-            n=self.rsa_keys.n,
-            e=self.rsa_keys.e)
+    def __check_token__(self, key):
+        if self.status == StatusCodes.connection_to_1C_server or self.status == StatusCodes.uploading:
+            SuccessToken.value = get_from_redis(SuccessToken.key_name)
+            return key == SuccessToken.value
 
-        self.rsa_keys.private = rsa.PrivateKey(
-            n=self.rsa_keys.n,
-            e=self.rsa_keys.e,
-            d=self.rsa_keys.d,
-            p=self.rsa_keys.p,
-            q=self.rsa_keys.q)
+    def __save_status_to_redis__(self, status_code: str) -> None:
+        if self.__is_status_code_correct__(status_code):
+            self.redis.set(name=StatusCodes.key_name, value=status_code)
+            self.status = status_code
 
-    def __render_new_keys__(self) -> None:
-        """ Generate new RSA keys and save it in to the redis"""
-        (self.rsa_keys.private, self.rsa_keys.public) = rsa.newkeys(nbits=self.encrypt_bits)
-        self.rsa_keys.n = self.rsa_keys.private.n
-        self.rsa_keys.e = self.rsa_keys.private.e
-        self.rsa_keys.d = self.rsa_keys.public.d
-        self.rsa_keys.p = self.rsa_keys.public.p
-        self.rsa_keys.q = self.rsa_keys.public.q
-
-        keys_dict = {
-            self.rsa_keys.key_name_n: self.rsa_keys.n,
-            self.rsa_keys.key_name_e: self.rsa_keys.e,
-            self.rsa_keys.key_name_d: self.rsa_keys.d,
-            self.rsa_keys.key_name_p: self.rsa_keys.p,
-            self.rsa_keys.key_name_q: self.rsa_keys.q,
-        }
-        self.redis.mset(keys_dict)
-
-    def __is_status_code_correct__(self, status_code) -> bool:
-        """
-        Check status_code
-        @param status_code: str arg from get_status()
-        @return: bool code status check result
-        """
-        if status_code == self.status_codes.uploaded: return True
-        if status_code == self.status_codes.connection: return True
-        if status_code == self.status_codes.uploading: return True
-        if status_code == self.status_codes.null: return True
-        return False
-
-    def set_status(self, status_code) -> str or object:
+    def set_status(self, status_code: str, success_token: str) -> None:
         """
         Save status_code in to the redis
-        @param status_code: str -> StatusCode.value
+        @param success_token: generated during integration init
+        @param status_code: StatusCode.value
         """
-        if self.__is_status_code_correct__(status_code):
-            self.redis.set(name=self.status_codes.key_name, value=status_code)
-            self.status = status_code
+        if self.__check_token__(success_token):
+            self.__save_status_to_redis__(status_code)
         else:
-            raise IOError(self.status_code_error)
+            raise IOError(Exceptions.wrong_token)
+
+    def start(self) -> None:
+        if self.status == StatusCodes.connection_to_1C_server:
+            json = {
+                SuccessToken.json_key: SuccessToken.value,
+                'action': StatusCodes.uploading
+            }
+            try:
+                # TODO verify=False! зарегистрировать сертификаты.
+                response = requests.post(url=HOST_1S, json=json, verify=False)
+                if response.status_code == requests.status_codes.codes.all_ok:
+                    # TODO сделать проверку ответов
+                    print('response.text: ', response.text)
+                else:
+                    raise IOError(Exceptions.connection_error)
+
+            except requests.exceptions.ConnectionError as error:
+                # TODO сделать логирование ошибок
+                self.__save_status_to_redis__(StatusCodes.connection_failed)
+                print('ERROR!', error)
 
 
 if __name__ == '__main__':
-    integration = IntegrationController()
-    integration.start()
+    IntegrationController(hot_start=True)
