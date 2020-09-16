3fcc9095dfd4d1f14bcef871efe4a1b68e8753ce
Hustoncun <1kor.oleg@gmail.com>
2020-09-16 16:44:10 +0300

    remove dependency_injector

--- config/containers.py
+++ config/containers.py
@@ -1,10 +1,10 @@
 """
-    Setup dependency injection file
+    dependency injection file
 """
 
-from dependency_injector import providers, containers
-from apps.integration_1S.tasks import IntegrationController
-
-
-class Integration1C(containers.DeclarativeContainer):
-    integration1c = providers.Singleton(IntegrationController)
+# from dependency_injector import providers, containers
+# from apps.integration_1S.tasks import IntegrationController
+#
+#
+# class Integration1C(containers.DeclarativeContainer):
+#     integration1c = providers.Singleton(IntegrationController)
